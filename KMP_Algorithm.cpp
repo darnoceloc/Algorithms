4954b1cfa79bf41539b3af423e4b931b12681961
darnoceloc <conrad.r.cole@gmail.com>
2019-10-11 21:00:23 -0400

    Add files via upload

---
+++ KMP_Algorithm.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//Simple match algorithm, not used
+bool has_substring(string& input_string, string& pattern){
+    int j=0;
+    int i=0;
+    int k = 0;
+    while(j<input_string.length() && i<pattern.length()){
+        if(input_string[j]== pattern[i]){
+        i++;
+        j++;
+        }else{i=0; k++ ; j=i;}}
+    if(i=pattern.length()){
+        return true;
+    }
+    return false;
+}
+
+//Function to compute temporary integer array for prefix/suffix matches
+vector<int> temp_array(string& pattern){
+    vector<int> tmp(pattern.length());
+    int index = 0;
+    tmp[0] = 0;
+    for(int i=1; i<pattern.length();){
+        if(pattern[i]==pattern[index]){
+            tmp[i]= tmp[index]+1;
+            index++;
+            i++;
+        }
+        else{
+            if(index != 0){
+            index = tmp[index-1];}
+            else{tmp[i]=0;
+            i++;}
+        }
+    }
+    return tmp; 
+}
+
+//Function to implement KMP Algorithm for pattern match
+bool KMP_alg(string& input_string, string& pattern){
+    vector<int> tmp_kmp = temp_array(pattern);
+    int i=0;
+    int j=0;
+    while(i < input_string.length() && j < pattern.length()){
+        if(input_string[i] == pattern[j]){
+            i++;
+            j++;}
+        else{
+            if(j!=0){
+            j = tmp_kmp[j-1];}
+            else{
+            i++;}
+        }
+    }
+    if(j == pattern.length()){
+        cout << "Match begins at char. " << i-pattern.length()+1 << " of input" << endl;
+        cout << "Match ends at char. " << i << " of input" << endl;
+        return true;}
+    else if(i == input_string.length()) {
+        cout <<"No match found" << endl;
+        return false;}
+    else {
+        cout << i <<" "<< j << endl;
+    }
+}
+
+int main(){
+    string input = "darnoc77eloc";
+    string pattern = "darnoc";
+    bool result = KMP_alg(input, pattern);
+    cout << result << endl;
+    return 0;
+}   

cfd953d386a0880c033aff3845b6a2038fcc628a
darnoceloc <conrad.r.cole@gmail.com>
2020-04-09 02:18:18 -0400

    Update heap_raw.cpp

--- heap_raw.cpp
+++ heap_raw.cpp
@@ -15,10 +15,10 @@ int readheap(int* theheap) {
     }
     for (int k = 1; k < i; ++k) {
     
-        if (theheap[k] < theheap[(k - 1) / 2])  { 
+        if (theheap[k] > theheap[(k - 1) / 2])  { 
             int j = k; 
            
-            while (theheap[j] < theheap[(j - 1) / 2])  { 
+            while (theheap[j] > theheap[(j - 1) / 2])  { 
                 std::swap(theheap[j], theheap[(j - 1) / 2]); 
                 j = (j - 1) / 2; 
             }
@@ -32,10 +32,10 @@ int readheap(int* theheap) {
         do { 
             index = (2 * j + 1);  
            
-            if (theheap[index] > theheap[index + 1] && index < (k - 1)) 
+            if (theheap[index] < theheap[index + 1] && index < (k - 1)) 
                 ++index; 
           
-            if (theheap[j] > theheap[index] && index < k) 
+            if (theheap[j] < theheap[index] && index < k) 
                 std::swap(theheap[j], theheap[index]); 
           
             j = index; 
@@ -50,16 +50,16 @@ void heapRemove(int* theheap, int& size) {
     for(int i=0; i<size-1; ++i){
         theheap[i] = theheap[i+1];
     }
-    int* x = theheap+size-1;
+    int* x = theheap;
     x = nullptr;
     delete x;
     size--; 
     for (int k = 1; k < size; ++k) {
         
-        if (theheap[k] < theheap[(k - 1) / 2])  { 
+        if (theheap[k] > theheap[(k - 1) / 2])  { 
             int j = k; 
              
-            while (theheap[j] < theheap[(j - 1) / 2])  { 
+            while (theheap[j] > theheap[(j - 1) / 2])  { 
                 std::swap(theheap[j], theheap[(j - 1) / 2]); 
                 j = (j - 1) / 2; 
             }
@@ -73,10 +73,10 @@ void heapRemove(int* theheap, int& size) {
         do { 
             index = (2 * j + 1);  
              
-            if (theheap[index] > theheap[index + 1] && index < (k - 1)) 
+            if (theheap[index] < theheap[index + 1] && index < (k - 1)) 
                 ++index; 
              
-            if (theheap[j] > theheap[index] && index < k) 
+            if (theheap[j] < theheap[index] && index < k) 
                 std::swap(theheap[j], theheap[index]); 
           
             j = index; 
@@ -86,7 +86,7 @@ void heapRemove(int* theheap, int& size) {
 }
 
 void heapPrint(int* theheap, int size) {
-    for (int i = size-1; i >= 0; --i){
+    for (int i = 0; i < size; ++i){
         std::cout << theheap[i] << ' ';
     }
     std::cout << '\n';

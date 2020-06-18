b47656bbfa53b2d7c8e697bd74ed8fe0be804df7
darnoceloc <conrad.r.cole@gmail.com>
2020-06-17 23:12:31 -0400

    Update heap_raw.cpp

--- heap_raw.cpp
+++ heap_raw.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <stdexcept>
-#include <cmath>
 
-int readheap(int* theheap) {
+int readHeap(int* theheap) {
     std::string num;
     int val = 0;
     int size = 0;
-    std::cout << "Enter the elements of heap" << std::endl;
     std::getline(std::cin, num);
     std::istringstream iss(num);    
     while (iss >> val) {
@@ -17,79 +14,33 @@ int readheap(int* theheap) {
             ++size;
         }
     }
-    if(size <= 1) {
-        throw std::runtime_error("Invalid user input");
+    if(size < 1) {
+        return -1;
     }
-
-for (int k = 1; k < size; ++k) {
-
-        if (theheap[k] > theheap[(k - 1) / 2])  { 
-            int j = k; 
-
-            while (theheap[j] > theheap[(j - 1) / 2])  { 
-                std::swap(theheap[j], theheap[(j - 1) / 2]); 
-                j = (j - 1) / 2; 
-            }
-        }
-    }
-    for (int k = size - 1; k > 0; --k) {
-        std::swap(theheap[0], theheap[k]);           
-
-        int j = 0, index; 
-
-        do { 
-            index = (2 * j + 1);  
-
-            if (theheap[index] < theheap[index + 1] && index < (k - 1)) 
-                ++index; 
-
-            if (theheap[j] < theheap[index] && index < k) 
-                std::swap(theheap[j], theheap[index]); 
-
-            j = index; 
-
-        } while (index < k); 
-    } 
-    std::cout << "Size of heap is " << size << '\n';
     return size;
 }
 
 void heapRemove(int* theheap, int& size) {
-    for(int i=0; i<size-1; ++i){
-        theheap[i] = theheap[i+1];
-    }
-    int* x = theheap;
-    x = nullptr;
-    delete x;
-    size--; 
-    for (int k = 1; k < size; ++k) {
-
-        if (theheap[k] > theheap[(k - 1) / 2])  { 
-            int j = k; 
-
-            while (theheap[j] > theheap[(j - 1) / 2])  { 
-                std::swap(theheap[j], theheap[(j - 1) / 2]); 
-                j = (j - 1) / 2; 
-            }
+    int index = 0;
+    theheap[index] = theheap[--size];
+    int l, r, smallest;
+    while(index < size){
+        l = index*2 + 1;
+        r = index*2 + 2;
+        smallest = index;
+        if(l < size && theheap[l] < theheap[smallest])
+            smallest = l;
+        if(r < size && theheap[r] < theheap[smallest])
+            smallest = r;
+        if (smallest != index)
+        {
+            int temp = theheap[index];
+            theheap[index] = theheap[smallest];
+            theheap[smallest] = temp;
+            index = smallest;
+            continue;
         }
-    }
-    for (int k = size - 1; k > 0; --k) {  
-        std::swap(theheap[0], theheap[k]);      
-
-        int j = 0, index; 
-
-        do { 
-            index = (2 * j + 1);  
-
-            if (theheap[index] < theheap[index + 1] && index < (k - 1)) 
-                ++index; 
-
-            if (theheap[j] < theheap[index] && index < k) 
-                std::swap(theheap[j], theheap[index]); 
-
-            j = index; 
-
-        } while (index < k); 
+        break;
     }
 }
 
